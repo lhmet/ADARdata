11fb2e0b796a906a002b8dc0fb3ac699e82242ec
lhmet <lhmet@ufsm.br>
2020-08-25 12:07:31 -0300

    improve doc

--- R/dados-sm.R
+++ R/dados-sm.R
@@ -7,22 +7,57 @@
 #'	@section Dados disponíbilizados:
 #'	Os dados são compostos de 3 quadro de dados:
 #'	\describe{
-#'			\item{clima_sm}{dados históricos diários}
-#'			\item{tempo_sm}{dados diários do último ano}
-#'			\item{recordes_atual_sm}{recordes dos dados do último ano}
+#'			\item{[clima_sm]}{dados históricos diários}
+#'			\item{[tempo_sm]}{dados diários do último ano}
+#'			\item{[recordes_atual_sm]}{recordes do último ano}
 #'	}
 #'	@format Todos os dados estão no formato [tibble][tibble::tibble-package].
 #'	@importFrom tibble tibble
 #'	@source	\url{https://bdmep.inmet.gov.br/}
-"clima_sm"
+"dados_sm"
 
-#'	Dados meteorológicos do ano atual
+#-------------------------------------------------------------------------------
+#'	Estatísticas diárias do clima de Santa Maria-RS
 #'
-#'	@format [tibble][tibble::tibble-package].
+#'	Estatísticas descritivas para cada dia do ano baseada na série histórica
+#'	dos dados climáticos da estação meteorológica do INMET em Santa Maria-RS.
+#'
+#'  @keywords datasets
+#'  @docType data
+#'  @format `clima_sm` consists of the following information in a
+#'  [tibble][tibble::tibble-package]  with 201 rows and 4 columns.
+#' 	\describe{
+#' 			\item{date}{data em formato POSIXct (formato de data e horário no R)}
+#' 			\item{tar}{temperatura do ar média diária em °C}
+#' 			\item{prec}{precipitação diária acumulada em mm}
+#' 			\item{dda}{dia do ano, varia de 1 a 365 (ou 366)}
+#' 	}
 #'	@importFrom tibble tibble
 #'	@source	\url{https://bdmep.inmet.gov.br/}
 "tempo_sm"
 
+#-------------------------------------------------------------------------------
+#'	Dados meteorológicos do último ano
+#'
+#'	Dados meteorológicos diários observados no último ano na estação
+#'	meteorológica do INMET em Santa Maria-RS.
+#'
+#'  @keywords datasets
+#'  @docType data
+#'  @format `tempo_sm` consists of the following information in a
+#'  [tibble][tibble::tibble-package]  with 201 rows and 4 columns.
+#' 	\describe{
+#' 			\item{date}{data em formato POSIXct (formato de data e horário no R)}
+#' 			\item{tar}{temperatura do ar média diária em °C}
+#' 			\item{prec}{precipitação diária acumulada em mm}
+#' 			\item{dda}{dia do ano, varia de 1 a 365 (ou 366)}
+#' 	}
+#'	@importFrom tibble tibble
+#'	@source	\url{https://bdmep.inmet.gov.br/}
+"tempo_sm"
+
+
+#-------------------------------------------------------------------------------
 #'	Recordes de temperatura e precipitação do último ano
 #'
 #'	@format [tibble][tibble::tibble-package].
